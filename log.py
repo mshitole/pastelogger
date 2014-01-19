0ceb7f84bb55e03d090f78159cfc63bea84084dc
maheshshitole <maheshshtl@gmail.com>
2014-01-19 19:48:21 +0530

    Logger class
    Added new log class which is wrapper around logger.
    Added log rotation handler.

---
+++ log.py
@@ -0,0 +1,60 @@
+# Author: Mahesh Shitole
+# Email : maheshshtl@gmail.com
+
+"""
+Author: Mahesh Shitole
+Contact: maheshshtl@gmail.com
+
+log.py contains wrapper around the logger
+
+This module handles the logging as well as log rotating
+Simple abstract around logger.
+
+Creating a singletone class neccessary here.
+"""
+
+# modules
+import logging
+
+# classes and methods
+
+class Singleton(type):
+    """ singleton class meta type
+    """
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,
+                                    **kwargs)
+        return cls._instances[cls]
+
+class Log(object):
+    """ log class definition
+    """
+    __metaclass__ = Singleton
+    def __init__(self, log_file_name, mode='a', max_bytes=10, backup_count=2):
+        """ Set up a specific logger with our desired output level
+        """
+        self.my_logger = logging.getLogger('MyLogger')
+        self.my_logger.setLevel(logging.DEBUG)
+        self.log_file_name = log_file_name
+        # Add the log message handler to the logger
+        handler = logging.handlers.RotatingFileHandler(
+              log_file_name, mode, max_bytes, backup_count)
+
+        self.my_logger.addHandler(handler)
+
+    def debug(self, msg):
+        """ debug level
+        """
+        self.my_logger.debug(msg)
+
+    def info(self, msg):
+        """ info level
+        """
+        self.my_logger.info(msg)
+
+    def error(self, msg):
+        """ error level
+        """
+        self.my_logger.error(msg)
